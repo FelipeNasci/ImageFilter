7d3a7170bfb97b7e5eebab4dde7d96a47a936fc2
Felipe Melo <ftiagomelo89@gmail.com>
2019-02-28 15:03:01 -0300

    Otimização filtros de brilho

--- Filters.py
+++ Filters.py
@@ -62,38 +62,44 @@ class Filter:
     def additiveBrightness(self, img, c):
         for i in range(0, len(img)):
             for l in range(0, len(img[0])):
-                if (img[i][l][0] + c > 255):
+                aux = img[i][l][0] + c
+                if (aux > 255):
                     img[i][l][0] = 255
                 else:
-                    img[i][l][0] = img[i][l][0] + c
+                    img[i][l][0] = aux
                 
-                if (img[i][l][1] + c > 255):
+                aux = img[i][l][1] + c
+                if (aux > 255):
                     img[i][l][1] = 255
                 else:
-                    img[i][l][1] = img[i][l][1] + c
-                
-                if (img[i][l][2] + c > 255):
+                    img[i][l][1] = aux
+
+                aux = img[i][l][2] + c
+                if (aux > 255):
                     img[i][l][2] = 255
                 else:
-                    img[i][l][2] = img[i][l][2] + c                         
+                    img[i][l][2] = aux                         
         return img
     
     #   Retorna a imagem com todos os pixels * c
     def multiplicativeBrightness(self, img, c):
         for i in range(0, len(img)):
             for l in range(0, len(img[0])):
-                if (img[i][l][0] * c > 255):
+                aux = img[i][l][0] * c
+                if (aux > 255):
                     img[i][l][0] = 255
                 else:
-                    img[i][l][0] = img[i][l][0] * c
+                    img[i][l][0] = aux
                 
-                if (img[i][l][1] * c > 255):
+                aux = img[i][l][1] * c
+                if (aux > 255):
                     img[i][l][1] = 255
                 else:
-                    img[i][l][1] = img[i][l][1] * c
+                    img[i][l][1] = aux
                 
-                if (img[i][l][2] * c > 255):
+                aux = img[i][l][2] * c
+                if (aux > 255):
                     img[i][l][2] = 255
                 else:
-                    img[i][l][2] = img[i][l][2] * c                         
+                    img[i][l][2] = aux                         
         return img
